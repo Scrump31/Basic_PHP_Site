0161efb9344f025b04203a688f4de9375d5f469d
Scottie Crump <scottiecrump@yahoo.com>
2016-06-21 15:46:49 -0400

    added code to display different category names

--- catalog.php
+++ catalog.php
@@ -1,10 +1,24 @@
 <?php $pageTitle = "Full Catalog"; ?>
 
 <?php $name = "Full Catalog"; ?>
+
+<?php 
+	if(isset($_GET["cat"])) {
+		if($_GET["cat"] === "books") {
+		$pageTitle = "Books";
+		
+		}else if($_GET["cat"] === "movies") {
+			$pageTitle = "Movies";
+		}else if($_GET["cat"] === "music") {
+			$pageTitle = "Music";
+		}
+	}
+?>
+
 <?php include 'include/header.php';?>
 
 <div class="section page">
-	<h1>Full Catalog</h1>
+	<h1><?php echo $pageTitle; ?></h1>
 </div><!-- section page -->
 
 <?php include 'include/footer.php';?>
