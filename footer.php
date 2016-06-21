80919e253baaf141a92de5a585216208933d2efb
Scottie Crump <scottiecrump@yahoo.com>
2016-06-21 14:44:53 -0400

    created initial page and header and footer templates

---
+++ footer.php
@@ -0,0 +1,12 @@
+	<!-- footer -->
+	<div class="footer">
+		<div class="wrapper">
+			<ul>		
+				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
+				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+			</ul>
+			<p>&copy;2015 Personal Media Library</p>
+		</div>
+	</div><!-- footer -->
+</body>
+</html>
