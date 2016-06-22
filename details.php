150aa7c9f4842171e6ada3401a4b45b0423eb373
Scottie Crump <scottiecrump@yahoo.com>
2016-06-22 18:52:44 -0400

    added item details to details page

--- details.php
+++ details.php
@@ -1,3 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+ini_set("html_errors", 1);
+?>
+
 <?php 
 	include("include/data.php");
 	include("include/functions.php");
@@ -29,5 +38,47 @@
 				<img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
 			</span>
 		</div><!-- media-picture -->
+		<div class="media-details">
+			<h1><?php echo $item["title"]; ?></h1>
+			<table>
+				<tr>
+					<th>Category</th>
+					<td><?php echo $item["category"]; ?></td>
+				</tr>
+				<tr>
+					<th>Genre</th>
+					<td><?php echo $item["genre"]; ?></td>
+				</tr>
+
+				<tr>
+					<th>Format</th>
+					<td><?php echo $item["format"]; ?></td>
+				</tr>
+
+				<tr>
+					<th>Year</th>
+					<td><?php echo $item["year"]; ?></td>
+				</tr>
+				<?php
+				if (strtolower($item["category"]) === "books") {
+					?>
+				<tr>
+					<th>Authors</th>
+					<td><?php echo implode(", ",$item["authors"]); ?></td>
+				</tr>
+				<tr>
+					<th>Publisher</th>
+					<td><?php echo $item["publisher"]; ?></td>
+				</tr>
+
+				<tr>
+					<th>ISBN</th>
+					<td><?php echo $item["isbn"]; ?></td>
+				</tr>
+
+
+				<?php } ?>
+			</table>
+		</div><!-- media-details -->
 	</div><!-- wrapper -->
 </div><!-- section page -->
