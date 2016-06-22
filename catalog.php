3622ad0fc712b6c032ec18c764373b94ba5854ce
Scottie Crump <scottiecrump@yahoo.com>
2016-06-22 13:45:05 -0400

    added 'Clean Code' to end of catalog array

--- catalog.php
+++ catalog.php
@@ -1,6 +1,14 @@
-<?php $pageTitle = "Full Catalog"; 
-
-      $name = "Full Catalog"; 
+<?php 
+	  $catalog = array();
+	  
+	  $catalog[] =  "Design Patterns";
+	  $catalog[] =  "Forrest Gump";
+	  $catalog[] = "Beethoven";
+	  $catalog[] = "Clean Code";	
+	  	
+	  $pageTitle = "Full Catalog"; 
+	  $section = null;
+      
 	  
  
 	if(isset($_GET["cat"])) {
@@ -20,8 +28,22 @@
 
 <?php include 'include/header.php';?>
 
-<div class="section page">
-	<h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+	
+	<div class="wrapper">
+		<h1><?php echo $pageTitle; ?></h1>
+		
+		<ul>
+		  <?php 
+			 foreach($catalog as $item) {
+			       echo "<li>" . $item . "</li>";               
+			  }
+		  ?>
+		</ul>
+		
+	</div><!-- wrapper -->
+	
+	
 </div><!-- section page -->
 
 <?php include 'include/footer.php';?>
