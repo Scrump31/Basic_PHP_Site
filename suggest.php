899407c4e8bf82ab2239877b71fff2de90720eca
Scottie Crump <scottiecrump@yahoo.com>
2016-06-24 10:42:43 -0400

    moved all form submission functionality to suggest. Deleted process and thanks php files

--- suggest.php
+++ suggest.php
@@ -1,16 +1,39 @@
 <?php 
+if($_SERVER["REQUEST_METHOD"] == "POST") {	
+		
+	$fname = $_POST["fname"];
+	$lname = $_POST["lname"];
+	$email = $_POST["email"];
+	$submit = $_POST["submit"];
+	
+	echo "<pre>";
+	$email_body = "";
+	$email_body .= "First Name: " . $fname . "\n";
+	$email_body .= "Last Name: " . $lname . "\n";
+	$email_body .= "Email: " . $email . "\n";
+	$email_body .= "Message: " . $submit . "\n";
+	echo $email_body;
+	echo "</pre>";
+	
+	// send email
+	header("location:suggest.php?status=thanks");
+}
+	
 	$pageTitle = "Suggest A Media Item"; 
 	$section = "suggest";
-	
-?>
+?>	
 
 <?php include 'include/header.php';?>
 
 <div class="section page">
 	<div class="wrapper">
 		<h1>Suggest a Media Item</h1>
+		<?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+				echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+			} else { ?>
+			
 		<p>If you think there is something I&rsquo;m missing, let me know! Please complete the form to send me an email.</p>
-		<form action="process.php" method="post">
+		<form action="suggest.php" method="post">
 		  <table>
 			  <tr>
 				  <th><label for="fname">First name:</label></th>
@@ -32,7 +55,7 @@
 		  </table>
 				  <td><input type="submit" value="Submit"></td>
 		</form>
-		
+		<?php } ?>
 	</div><!-- wrapper -->
 </div><!-- section page -->
 
